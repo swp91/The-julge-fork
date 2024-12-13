aa5e3f413c5d2bb99399f7405fc07c64abdcce64
홍지윤 <h_zhirun@naver.com>
2024-12-13 18:50:56 +0900

    Feat: 공통 Input 컴포넌트 구현

---
+++ src/app/_components/Input.tsx
@@ -0,0 +1,37 @@
+interface InputProps {
+  type: string;
+  value: string; // 상태값 지정
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  className?: string; // 스타일 추가 지정
+  label?: string; // 라벨 표시만을 위한 props
+  placeholder?: string;
+  error?: string;
+}
+
+export const Input: React.FC<InputProps> = ({
+  type,
+  value,
+  onChange,
+  className,
+  label,
+  placeholder,
+  error,
+}) => {
+  return (
+    <div className={`flex flex-col gap-2 ${className || ''}`}>
+      {label && (
+        <label className="w-full block text-16 text-base text-black">
+          {label}
+        </label>
+      )}
+      <input
+        className={`w-full py-4 pl-5 border rounded-md ${error ? 'border-red-40' : 'border-gray-300'} focus:border-black focus:outline-none focus:ring-0`}
+        type={type}
+        value={value}
+        onChange={onChange}
+        placeholder={placeholder}
+      />
+      {error && <p className="w-full px-2 text-12 text-red-40">{error}</p>}
+    </div>
+  );
+};
