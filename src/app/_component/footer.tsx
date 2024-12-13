37da0ca2fc8161a1ec8bf83f1e17c7a923e9db3e
Stemplun <dnfl311@pusan.ac.kr>
2024-12-13 20:58:43 +0900

    Footer 컴포넌트 추가

---
+++ src/app/_component/footer.tsx
@@ -0,0 +1,27 @@
+import Image from 'next/image';
+export default function Footer() {
+  return (
+    <div className="flex justify-around h-24 bg-gray-100 items-center text-base">
+      <p className="text-gray-500">©codeit - 2024</p>
+      <div className="flex justify-around gap-7 text-gray-500">
+        <p>Privacy Policy</p>
+        <p>FAQ</p>
+      </div>
+      <div className="flex justify-around gap-2">
+        <Image
+          src={'/image/envelope-square.svg'}
+          alt=""
+          width={24}
+          height={24}
+        />
+        <Image
+          src={'/image/facebook-square.svg'}
+          alt=""
+          width={24}
+          height={24}
+        />
+        <Image src={'/image/instagram.svg'} alt="" width={24} height={24} />
+      </div>
+    </div>
+  );
+}
