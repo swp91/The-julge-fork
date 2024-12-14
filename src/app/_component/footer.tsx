19364bf8ab2bf288a523495e96a8c99434bd9d1b
Stemplun <dnfl311@pusan.ac.kr>
2024-12-14 11:20:48 +0900

    반응형 추가
    - 375px(모바일)버전에서는 구조가 변경이 되어 하나의 코드로는 하기 힘들다 판단했습니다.
    - 그래서 useState와 useEffect를 활용하여 현재 윈도우의 width를 측정하여 640px을 기준으로 각각 다른 코드를 작성하였습니다.

--- src/app/_component/footer.tsx
+++ src/app/_component/footer.tsx
@@ -1,33 +1,87 @@
+'use client';
+
+import { useState, useEffect } from 'react';
 import Image from 'next/image';
 
 export default function Footer() {
+  const [windowWidth, setWindowWidth] = useState(0);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setWindowWidth(window.innerWidth);
+    };
+
+    handleResize();
+
+    window.addEventListener('resize', handleResize);
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
   return (
-    <div className="flex justify-around h-24 bg-gray-100 items-center text-base">
-      <p className="text-gray-500">©codeit - 2024</p>
-      <div className="flex justify-around gap-7 text-gray-500">
-        <p>Privacy Policy</p>
-        <p>FAQ</p>
-      </div>
-      <div className="flex justify-around gap-2">
-        <Image
-          src={'/image/envelope-square.svg'}
-          alt="e-mail"
-          width={24}
-          height={24}
-        />
-        <Image
-          src={'/image/facebook-square.svg'}
-          alt="facebook"
-          width={24}
-          height={24}
-        />
-        <Image
-          src={'/image/instagram.svg'}
-          alt="instagram"
-          width={24}
-          height={24}
-        />
-      </div>
+    <div className="bg-gray-100 text-base">
+      {windowWidth >= 640 ? (
+        <div className="sm:flex sm:justify-around sm:items-center sm:h-24">
+          <p className="text-gray-500 text-center sm:text-left">
+            ©codeit - 2024
+          </p>
+          <div className="flex justify-center gap-7 text-gray-500 sm:justify-around">
+            <p>Privacy Policy</p>
+            <p>FAQ</p>
+          </div>
+          <div className="flex justify-center gap-4">
+            <Image
+              src={'/image/envelope-square.svg'}
+              alt="e-mail"
+              width={24}
+              height={24}
+            />
+            <Image
+              src={'/image/facebook-square.svg'}
+              alt="facebook"
+              width={24}
+              height={24}
+            />
+            <Image
+              src={'/image/instagram.svg'}
+              alt="instagram"
+              width={24}
+              height={24}
+            />
+          </div>
+        </div>
+      ) : (
+        <div className="flex justify-around gap-7 text-gray-500 h-32">
+          <div className="flex flex-col justify-center gap-5">
+            <div className="flex gap-4">
+              <p>Privacy Policy</p>
+              <p>FAQ</p>
+            </div>
+            <p className="text-gray-500 text-left">©codeit - 2024</p>
+          </div>
+          <div className="flex gap-4 mb-12">
+            <Image
+              src={'/image/envelope-square.svg'}
+              alt="e-mail"
+              width={24}
+              height={24}
+            />
+            <Image
+              src={'/image/facebook-square.svg'}
+              alt="facebook"
+              width={24}
+              height={24}
+            />
+            <Image
+              src={'/image/instagram.svg'}
+              alt="instagram"
+              width={24}
+              height={24}
+            />
+          </div>
+        </div>
+      )}
     </div>
   );
 }
