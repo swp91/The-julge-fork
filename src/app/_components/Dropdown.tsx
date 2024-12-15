b2ff5c9cc59e32ae6716d5431672e3d6a1dc8cb9
홍지윤 <h_zhirun@naver.com>
2024-12-15 13:18:52 +0900

    Feat: Dropdown 외부 클릭시 닫힘 기능 추가 및 props 수정

--- src/app/_components/Dropdown.tsx
+++ src/app/_components/Dropdown.tsx
@@ -1,57 +1,81 @@
 import clsx from 'clsx';
 import Image from 'next/image';
+import { useEffect, useRef, useState } from 'react';
 
 interface DropdownProps {
-  options: string[]; // 드롭다운 옵션
-  selectedOption: string | null; // 선택된 옵션, 선택되지 않은 경우 null
-  onOptionClick: (value: string) => void; // 옵션 클릭시 실행 함수
-  isOpen: boolean; // 드롭다운 메뉴가 열려있는지
-  toggleDropdown: () => void; // 드롭다운 열림/닫힘 상태 전환
+  options: string[];
+  value?: string;
+  onChange?: (value: string) => void;
   className?: string;
   placeholder?: string;
 }
 
 export const Dropdown = ({
   options,
-  selectedOption,
-  onOptionClick,
-  isOpen,
-  toggleDropdown,
-  placeholder,
+  value,
+  onChange,
   className,
+  placeholder = '선택',
 }: DropdownProps) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const dropdownRef = useRef<HTMLDivElement>(null);
+
+  // 드롭다운 외부 클릭시 닫기
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (
+        dropdownRef.current &&
+        !dropdownRef.current.contains(event.target as Node)
+      ) {
+        setIsOpen(false);
+      }
+    };
+
+    // 드롭다운이 열려 있을 때만 이벤트 리스너 추가
+    if (isOpen) document.addEventListener('mousedown', handleClickOutside);
+    return () => document.removeEventListener('mousedown', handleClickOutside);
+  }, [isOpen]);
+
+  const toggleDropdown = () => setIsOpen((prev) => !prev);
+
+  const handleOptionClick = (option: string) => {
+    onChange?.(option);
+    setIsOpen(false);
+  };
+
   return (
-    <div className={clsx('relative', className)}>
+    <div className={clsx('relative', className)} ref={dropdownRef}>
       <button
         onClick={toggleDropdown}
         className={clsx(
           'w-full flex items-center justify-between bg-white border border-gray-300 rounded py-4 px-5 text-left',
-          selectedOption ? 'text-black' : 'text-gray-400',
+          value ? 'text-black' : 'text-gray-400',
         )}>
-        {selectedOption || placeholder}
+        {value || placeholder}
         <div className={clsx('transform', { 'rotate-180': isOpen })}>
           <Image
             src={'/image/dropdown-triangle.svg'}
             alt="드롭다운 화살표"
             width={16}
             height={16}
+            priority
           />
         </div>
       </button>
+
       {isOpen && (
-        // 스크롤바 padding 넣는 방법
         <ul
           className={clsx(
             'custom-scrollbar absolute z-2 w-full bg-white border rounded mt-2 max-h-[230px] overflow-auto',
           )}>
-          {options.map((option, index) => (
+          {options.map((option) => (
             <li
-              key={index}
+              key={option}
               className={clsx(
                 'py-3 text-14 text-black text-center cursor-pointer border-b',
                 'hover:bg-blue-10 border-gray-200',
               )}
-              onClick={() => onOptionClick(option)}>
+              onClick={() => handleOptionClick(option)}>
               {option}
             </li>
           ))}
