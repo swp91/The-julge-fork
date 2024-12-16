b857719b5bafa3ade306aec6feae7448f3ac14df
홍지윤 <h_zhirun@naver.com>
2024-12-16 15:03:56 +0900

    Feat: 모달 환경설정(modalConfig.ts) 생성

---
+++ src/app/config/modalConfig.ts
@@ -0,0 +1,18 @@
+export const modalConfig = {
+  success: {
+    icon: null,
+    responsiveClass:
+      'w-[100%] sm:w-[327px] lg:w-[540px] h-auto sm:h-[220px] lg:h-[250px]',
+    buttonLayout: 'justify-end',
+  },
+  alert: {
+    icon: '/image/modal-alert.svg',
+    responsiveClass: 'w-[100%] w-[298px]',
+    buttonLayout: 'justify-center',
+  },
+  question: {
+    icon: '/image/modal-question.svg',
+    responsiveClass: 'w-[100%] w-[298px]',
+    buttonLayout: 'justify-center',
+  },
+};
