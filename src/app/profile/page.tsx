cfd495262a8192099ac40e5f5ae8f7f665ef8386
Stemplun <dnfl311@pusan.ac.kr>
2024-12-21 19:20:47 +0900

    프로필 상세 페이지 기본 구성

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,5 +1,34 @@
+'use client';
+
+import { useState } from 'react';
+import Footer from '../_components/Footer';
+import Header from '../_components/Header';
+import PostProfile from '../_components/PostProfile';
+
 const ProfilePage = () => {
-  return <>프로필 페이지</>;
+  const [profileStatus, setProfileStatus] = useState(false);
+  const [applicationStatus, setApplicationStatus] = useState(false);
+
+  return (
+    <div>
+      <Header />
+      <div>
+        {profileStatus ? (
+          <div></div>
+        ) : (
+          <PostProfile isExist={profileStatus} type={'profile'} />
+        )}
+      </div>
+      <div>
+        {applicationStatus ? (
+          <div></div>
+        ) : (
+          <PostProfile isExist={applicationStatus} type={'application'} />
+        )}
+      </div>
+      <Footer />
+    </div>
+  );
 };
 
 export default ProfilePage;
