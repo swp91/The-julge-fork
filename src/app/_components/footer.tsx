fb0ef27b1beef5aeed903787c2ad2a4a19bc4ff2
Stemplun <dnfl311@pusan.ac.kr>
2024-12-30 10:19:48 +0900

    footer 컴포넌트 이미지 컴포넌트 수정

--- src/app/_components/footer.tsx
+++ src/app/_components/footer.tsx
@@ -20,19 +20,19 @@ const Footer = () => {
               src={'/image/envelope-square.svg'}
               alt='e-mail'
               width={24}
-              height={24}
+              fill
             />
             <Image
               src={'/image/facebook-square.svg'}
               alt='facebook'
               width={24}
-              height={24}
+              fill
             />
             <Image
               src={'/image/instagram.svg'}
               alt='instagram'
               width={24}
-              height={24}
+              fill
             />
           </div>
         </div>
@@ -50,19 +50,19 @@ const Footer = () => {
               src={'/image/envelope-square.svg'}
               alt='e-mail'
               width={24}
-              height={24}
+              fill
             />
             <Image
               src={'/image/facebook-square.svg'}
               alt='facebook'
               width={24}
-              height={24}
+              fill
             />
             <Image
               src={'/image/instagram.svg'}
               alt='instagram'
               width={24}
-              height={24}
+              fill
             />
           </div>
         </div>
