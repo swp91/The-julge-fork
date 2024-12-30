41b5114b51861ae9ec45eaa65b80cdcc856c0224
Stemplun <dnfl311@pusan.ac.kr>
2024-12-30 10:30:25 +0900

    검색 페이지 생성

---
+++ src/app/search/page.tsx
@@ -0,0 +1,13 @@
+import Footer from '../_components/Footer';
+import Header from '../_components/Header';
+
+const SearchPage = () => {
+  return (
+    <div>
+      <Header />
+      <div></div>
+      <Footer />
+    </div>
+  );
+};
+export default SearchPage;
