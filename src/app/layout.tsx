01be9378ab3cfa54f2b1904cbefece5be470afbc
swp91 <seongwoo4477@gmail.com>
2024-12-31 06:39:36 +0900

    사이트 이름, 파비콘설정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,13 @@
-import { AppProvider } from './_context/AppContext';
-import './globals.css';
+import { AppProvider } from "./_context/AppContext";
+import "./globals.css";
+
+export const metadata = {
+  title: "더 줄게",
+  description: "파트타임 아르바이트 맞춤 매칭플랫폼 더 줄게에서 찾아보세요.",
+  icons: {
+    icon: "/image/favicon.svg",
+  },
+};
 
 export default function RootLayout({
   children,
@@ -7,7 +15,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang='ko'>
+    <html lang="ko">
       <body>
         <AppProvider>{children}</AppProvider>
       </body>
